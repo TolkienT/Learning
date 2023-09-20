63a90d69bd60256f5f094dc90d5cacd1c74cd668
tang.zx <tang.zx@cidi.ai>
2023-09-20 17:11:43 +0800

    前端提交

---
+++ vue/webapi.vue3/src/views/Settings/SysParam.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <!--工具条-->
+        <el-col>
+            <el-button :type="primary">按键</el-button>
+        </el-col>
+
+        <el-table style="width: 100%">
+            <el-table-column type="index" label="序号" width="80"> </el-table-column>
+            <el-table-column prop="param_code" label="参数编码" width=""> </el-table-column>
+            <el-table-column prop="param_name" label="参数名称" width=""> </el-table-column>
+        </el-table>
+
+        <el-col :span="24">
+            <el-pagination layout="prev,pager,next" :page-size="50"></el-pagination>
+        </el-col>
+    </section>
+</template>
+
+<style>
+#app .header {
+    height: 60px;
+    line-height: 60px;
+    background: #2f3e52;
+    color: #fff;
+}
+</style>
